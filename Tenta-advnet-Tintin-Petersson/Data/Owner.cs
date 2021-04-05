c20121a7bc701c96a0610d7c19d5c81872d0f101
TintinPetersson <79974127+TintinPetersson@users.noreply.github.com>
2021-04-05 12:49:40 +0200

    Fixed owner prop

--- Tenta-advnet-Tintin-Petersson/Data/Owner.cs
+++ Tenta-advnet-Tintin-Petersson/Data/Owner.cs
@@ -5,8 +5,7 @@ namespace Tenta_advnet_Tintin_Petersson
     public class Owner
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string Name { get; set; }
         public virtual ICollection<Hamster> Hamsters { get; set; }
 
     }
